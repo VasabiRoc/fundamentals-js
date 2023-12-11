9580fdd7e6add669ba0f59b6021b4c6e42a33d43
VasabiRoc <147431082+VasabiRoc@users.noreply.github.com>
2023-12-11 17:09:37 +0000

    1-4

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+export const concatStrings = (a , b) => a + b;
 
+export const isString = (value) => {
+    if (typeof value === 'string') {
+        return true
+    } 
+    return false
+};
+
+export const identifySign = (n) => {
+    if (n === 0) {
+        return "Ноль"
+    }
+    if (n < 0) {
+        return "Отрицательное число"
+    }
+    if(n > 0) {
+        return "Положительное число"
+    }
+};
+
+export const reverseWords = (text) => text.split(' ').reverse().join(' ');
+
+export const wordsCount = (text) => {
+    let result = '';
+    
+  return
+};
